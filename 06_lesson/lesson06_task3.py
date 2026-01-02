918bb3932812e4432ce01409fee1fc1a0b49d38a
katyafenich <katyfenich123@gmai.com>
2026-01-02 19:25:03 +0300

    lesson06_task3.py

--- 06_lesson/lesson06_task3.py
+++ 06_lesson/lesson06_task3.py
@@ -1,18 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 
 driver = webdriver.Chrome()
 driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
-
 waiter = WebDriverWait(driver, 15, 0.1)
+
+
 waiter.until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, "#landscape"))
+    lambda driver: len(driver.find_elements(By.CSS_SELECTOR, "#image-container img")) == 4
 )
 
-src = driver.find_element(By.CSS_SELECTOR, "#award").get_attribute("src")
+
+all_images = driver.find_elements(By.CSS_SELECTOR, "#image-container img")
+print(f"Загружено картинок: {len(all_images)}")
+
+
+src = all_images[2].get_attribute("src")
 print(src)
 
 
